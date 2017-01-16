30560fceefc14e84f63a3b53b5107cabdb427530
GhostViz <thejoenichols@gmail.com>
2017-01-15 22:01:43 -0700

    Adding Web Sockets

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {return true},
+}
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -11,5 +18,21 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from go</h1>")
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		msgType, msg, err := socket.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(msg))
+		if err = socket.WriteMessage(msgType, msg); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
